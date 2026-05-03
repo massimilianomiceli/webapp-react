653e5334f79d0fade46e2a4ffca8f8ab056a221c
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-05-03 12:20:44 +0200

    definita struttura rotte

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./layouts/Defaultlayout";
+import HomePage from "./components/HomePage";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies/:id" element={<HomePage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
