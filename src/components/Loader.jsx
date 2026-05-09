b01adf80a2024cd0d01b32905625761b6f2e5da2
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-05-09 17:58:18 +0200

    creazione del componente loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <div className="overlay">
+      <div className="spinner-border text-primary" role="status"></div>
+    </div>
+  );
+}
+
+export default Loader;
